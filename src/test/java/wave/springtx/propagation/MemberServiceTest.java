64508b36e4caed0d59c49299e6877f3ab2bda5cd
waveofmymind <sjun0913@gmail.com>
2023-02-05 19:49:13 +0900

    feat: 논리 트랜잭션 런타임 에러 처리

--- src/test/java/wave/springtx/propagation/MemberServiceTest.java
+++ src/test/java/wave/springtx/propagation/MemberServiceTest.java
@@ -69,4 +69,24 @@ class MemberServiceTest {
         Assertions.assertTrue(logRepository.find(username).isPresent());
     }
 
+    /**
+     * memberService    @Transactional:ON
+     * memberRepository @Transactional:ON
+     * logRepository    @Transactional:ON Exception
+     */
+    @Test
+    void outerTxOn_fail() {
+        //given
+        String username ="로그예외_outerTxOn_fail";
+
+        //when
+        org.assertj.core.api.Assertions.assertThatThrownBy(() -> memberService.joinV1(username))
+                        .isInstanceOf(RuntimeException.class);
+
+        //then: 모든 데이터가 롤백된다.
+        Assertions.assertTrue(memberRepository.find(username).isPresent());
+        Assertions.assertTrue(logRepository.find(username).isPresent());
+    }
+
+
 }
