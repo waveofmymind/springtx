56936251cfb1b77f49dcea9b9f1c47c1e377f44e
waveofmymind <sjun0913@gmail.com>
2023-02-05 18:31:31 +0900

    feat: Member,Log 엔티티를 이용한 트랜잭션 전파 활용 예제 테스트

---
+++ src/test/java/wave/springtx/propagation/MemberServiceTest.java
@@ -0,0 +1,38 @@
+package wave.springtx.propagation;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+@SpringBootTest
+class MemberServiceTest {
+
+    @Autowired MemberRepository memberRepository;
+    @Autowired MemberService memberService;
+    @Autowired LogRepository logRepository;
+
+    /**
+     * memberService    @Transactional:OFF
+     * memberRepository @Transactional:ON
+     * logRepository    @Transactional:ON
+     */
+    @Test
+    void outerTxOff_success() {
+        //given
+        String username ="outerTxOff_success";
+
+        //when
+        memberService.joinV1(username);
+
+        //then: 모든 데이터가 정상 저장된다.
+        Assertions.assertTrue(memberRepository.find(username).isPresent());
+        Assertions.assertTrue(logRepository.find(username).isPresent());
+
+
+    }
+}
